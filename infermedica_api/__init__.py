03162c2149502f7d1b2e47b780738a399afd05cb
Arkadiusz Szydełko <arekxd@gmail.com>
2017-07-27 15:19:31 +0200

    Bump version.

--- infermedica_api/__init__.py
+++ infermedica_api/__init__.py
@@ -36,7 +36,7 @@ is available at <https://developer.infermedica.com>.
 """
 
 __title__ = 'Infermedica API'
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 __author__ = 'Arkadiusz Szydelko'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2017 Infermedica'
